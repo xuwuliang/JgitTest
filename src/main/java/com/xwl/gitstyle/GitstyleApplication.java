005910fbdcaf884a6ffe76190013065ff34b7469
xuwuliang <2568024059@qq.com>
2020-10-22 09:18:37 +0800

    第一次提交

---
+++ src/main/java/com/xwl/gitstyle/GitstyleApplication.java
@@ -0,0 +1,13 @@
+package com.xwl.gitstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xwl/gitstyle/GitstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xwl.gitstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
